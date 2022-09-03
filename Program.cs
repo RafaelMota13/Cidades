7dc0c14fbf379a9e3e123b4a440cf9bd9c9b5d48
Rafael Mota <faelmota7@gmail.com>
2022-09-03 16:38:34 +0000

    Resolução

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine();
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Graande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine();
